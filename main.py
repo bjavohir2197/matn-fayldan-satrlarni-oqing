2db20eb2a5a6f2f269d9b17a2a211ce4321c20dd
bjavohir2197 <bjavohir2197@gmail.com>
2026-04-18 12:29:40 +0500

    🚀 Add main application code

---
+++ main.py
@@ -0,0 +1,10 @@
+def main():
+        print("=" * 40)
+    print("Masala: Matn fayldan satrlarni oqing va faqat palindrom satrlarni yangi faylga yozing.")
+    print("=" * 40)
+    print("\nDastur muvaffaqiyatli ishga tushdi!")
+    print("Masala yechildi.")
+
+
+if __name__ == "__main__":
+    main()
